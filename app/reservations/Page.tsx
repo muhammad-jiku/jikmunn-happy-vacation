d434d09775b4b8c0c90b27f1dae419c47c8ccf26
muhammad-jiku <muhammadjiku364@gmail.com>
2023-07-06 21:52:45 +0600

    reservations page

---
+++ app/reservations/Page.tsx
@@ -0,0 +1,39 @@
+import getCurrentUser from '@/actions/getCurrentUser';
+import TripsClient from './ReservationsClient';
+import getReservations from '@/actions/getReservations';
+import { ClientOnly, EmptyState } from '@/components';
+
+const Page = async () => {
+  const currentUser = await getCurrentUser();
+
+  if (!currentUser) {
+    return (
+      <ClientOnly>
+        <EmptyState title='Unauthorized' subtitle='Please login' />
+      </ClientOnly>
+    );
+  }
+
+  const reservations = await getReservations({
+    authorId: currentUser.id,
+  });
+
+  if (reservations.length === 0) {
+    return (
+      <ClientOnly>
+        <EmptyState
+          title='No reservations found'
+          subtitle='Looks like you have no reservations on your properties.'
+        />
+      </ClientOnly>
+    );
+  }
+
+  return (
+    <ClientOnly>
+      <TripsClient reservations={reservations} currentUser={currentUser} />
+    </ClientOnly>
+  );
+};
+
+export default Page;
