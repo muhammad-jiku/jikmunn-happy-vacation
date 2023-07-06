ab1db08d225474f538bbe55099fd5b5396e29792
muhammad-jiku <muhammadjiku364@gmail.com>
2023-07-06 20:39:44 +0600

    properties page

---
+++ app/properties/page.tsx
@@ -0,0 +1,33 @@
+import { ClientOnly, EmptyState } from '@/components';
+import PropertiesClient from './PropertiesClient';
+import getCurrentUser from '@/actions/getCurrentUser';
+import getListings from '@/actions/getListings';
+
+const Page = async () => {
+  const currentUser = await getCurrentUser();
+
+  if (!currentUser) {
+    return <EmptyState title='Unauthorized' subtitle='Please login' />;
+  }
+
+  const listings = await getListings({ userId: currentUser.id });
+
+  if (listings.length === 0) {
+    return (
+      <ClientOnly>
+        <EmptyState
+          title='No properties found'
+          subtitle='Looks like you have no properties.'
+        />
+      </ClientOnly>
+    );
+  }
+
+  return (
+    <ClientOnly>
+      <PropertiesClient listings={listings} currentUser={currentUser} />
+    </ClientOnly>
+  );
+};
+
+export default Page;
